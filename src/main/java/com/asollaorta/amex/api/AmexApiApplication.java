4c63a27e5fe0f333b7d3cbbbd255d201ca7cc945
medicensolla <andres.solla@gmail.com>
2024-10-23 13:46:41 -0400

    Start project

---
+++ src/main/java/com/asollaorta/amex/api/AmexApiApplication.java
@@ -0,0 +1,13 @@
+package com.asollaorta.amex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asollaorta/amex/api/AmexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asollaorta.amex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
